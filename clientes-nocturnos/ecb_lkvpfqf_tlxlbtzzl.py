cc4934c65ab26bd82b77d536ea351cbd7e392887
Roger Fuentes <rogelio.fuentes@globant.com>
2016-06-23 13:44:55 +0000

    Commit changes

---
+++ clientes-nocturnos/ecb_lkvpfqf_tlxlbtzzl.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""ecb_lkvpfqf_tlxlbtzzl generator."""
+
+import clarity
+
+
+class MyAttribute(clarity.ClarityAttribute):
+
+    def build(self):
+        # Get output name based on class and env.
+        output_name = self.clarity_output_name()
+
+        # Create build sql
+        creator_query = "create table {} (PersonID int,LastName varchar(255))".format(
+            output_name
+        )
+
+        self.clarity.logger.debug(
+            "Build attribute {}. Create table with name {}.".format(
+                self.source_file, output_name)
+        )
+
+        # Drop table here.
+        self.clarity.drop_table(output_name)
+
+        # Do hive.
+        self.clarity.do_hive(creator_query)
+
+
+if __name__ == "__main__":
+
+    attr = MyAttribute()
